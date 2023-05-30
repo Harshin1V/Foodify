f08623bee2dd56c0d5e095b179b8e491d01b0f4d
harshini1v <harshinivutukuri1@gmail.com>
2023-05-30 16:11:14 +0530

    updated

--- Food-Delivery-App/src/components/Contact.tsx
+++ Food-Delivery-App/src/components/Contact.tsx
@@ -2,7 +2,7 @@ import React from "react";
 export default function Contact() {
   return (
     <h1 className=" text-center p-3 text-3xl font-semibold">
-      Contact Us at thisisyashgarg@gmail.com
+      Contact Us at harshinivutukuri1@.com
     </h1>
   );
 }
